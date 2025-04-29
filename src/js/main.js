73e496cb9a1549844cfb2408f7f548d3a77063b6
perez-ben <twobees12@gmail.com>
2025-04-29 11:54:17 -0600

    completed the first part of making it dynamic

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+const disclaimer = document.querySelector(".disclaimer > a");
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
+
+function parkInfoTemplate(info) {
+    return `<a href="/" class="hero-banner_title">${info.name}</a>
+    <p class="hero-banner_subtitle">
+      <span>${info.designation}</span>
+      <span>${info.states}</span>
+    </p>`;
+}
