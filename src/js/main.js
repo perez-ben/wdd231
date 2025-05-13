eeb3d7017a1f5a958c6729eb8e7fb18e69d79d2d
perez-ben <twobees12@gmail.com>
2025-05-13 00:59:56 -0600

    updated parkService export of park data and cleand up index.html

--- src/js/main.js
+++ src/js/main.js
@@ -23,27 +23,6 @@ function setHeaderInfo(parkData) {
   document.querySelector(".hero-banner__content").innerHTML = parkInfoTemplate(parkData);
 }
 
-const parkInfoLinks = [
-  {
-    name: "Current Conditions &#x203A;",
-    link: "conditions.html",
-    image: parkData.images[2].url,
-    description:
-    "See what conditions to expect in the park before leaving on your trip!"
-  },
-  {
-    name: "Fees and Passes &#x203A;",
-    link: "fees.html",
-    image: parkData.images[3].url,
-    description: "Learn about the fees and passes that are available."
-  },
-  {
-    name: "Visitor Centers &#x203A;",
-    link: "visitor_centers.html",
-    image: parkData.images[9].url,
-    description: "Learn about the visitor centers in the park."
-  }
-];
 
 function mediaCardTemplate(info) {
   return `<div class="media-card">
