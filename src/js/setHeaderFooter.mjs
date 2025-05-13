05ca930e7df17332a9ba228b106ff24e8f4fbe34
perez-ben <twobees12@gmail.com>
2025-05-13 14:09:17 -0600

    fixed parkData variable swap back to data

--- src/js/setHeaderFooter.mjs
+++ src/js/setHeaderFooter.mjs
@@ -1,21 +1,21 @@
 import { parkInfoTemplate, footerTemplate } from "./templates.mjs";
 
-function setHeaderInfo(parkData) {
+function setHeaderInfo(data) {
     //insert data into disclaimerj section
     const disclaimer = document.querySelector(".disclaimer > a");
-    disclaimer.href = parkData.url;
-    disclaimer.innerHTML = parkData.fullName;
+    disclaimer.href = data.url;
+    disclaimer.innerHTML = data.fullName;
     //update the title of the site. Notice that we can select things in the head just like in the body with querySelector
-    document.querySelector("head > title").textContent = parkData.fullName;
+    document.querySelector("head > title").textContent = data.fullName;
     // set the banner image
-    document.querySelector(".hero-banner > img").src = parkData.images[0].url;
+    document.querySelector(".hero-banner > img").src = data.images[0].url;
     // use the template function above to set the rest of the park specific info in the header
     document.querySelector(".hero-banner__content").innerHTML = parkInfoTemplate(data);
 }
 
-function setFooter(parkData) {
+function setFooter(data) {
     const footerEl = document.querySelector("#park-footer");
-    footerEl.insertAdjacentHTML("afterbegin", footerTemplate(parkData));
+    footerEl.insertAdjacentHTML("afterbegin", footerTemplate(data));
 }
 
 export default function setHeaderFooter(parkData) {
